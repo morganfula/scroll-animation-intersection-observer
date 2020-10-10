6d253a1532b1ef28f6aee5ec30ce2f09b18d4650
morganfula <morgan.fula@me.com>
2020-10-10 20:31:39 +0200

    playing with animations

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ observer = new IntersectionObserver((entries) => {
 
   entries.forEach((entry) => {
     if (entry.intersectionRatio > 0) {
-      entry.target.style.animation = `anim1 1.5s ${entry.target.dataset.delay} forwards ease-out`;
+      entry.target.style.animation = `anim1 3s ${entry.target.dataset.delay} forwards ease-out`;
     } else {
       entry.target.style.animation = 'none';
     }
